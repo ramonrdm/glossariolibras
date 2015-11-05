9d3a49579365f2f18d6588f544ad18f5c7f9dc2b
joao pedro villegas <joao.pgkv@gmail.com>
2015-11-05 16:35:33 -0200

    models algumas mudanças classe Sinal

--- glossario/models.py
+++ glossario/models.py
@@ -1,18 +1,26 @@
 from django.db import models
+from django.core.files import File
 
 # Create your models here.
 
 class Glossario(models.Model):
 	nome = models.CharField(max_length=100)
-	
-
-
+	grupoUsuarios = ForeignKey(Grupo)
 
 class Sinal(models.Model):
+	glossario = models.ForeignKey(Glossario)
+	traducaoP = models.CharField(max_length=30)
+	traducaoI = models.CharField(max_length=30)
+	bsw = models.TextField()
+	descricao = models.CharField(max_length=50)
+	localizacao = models.ForeignKey(Localizacao)
+	dataPost = models.DateField()
+	publicado = models.BooleanField()
+	videoSinal = models.ForeignKey(Video)
+	videoDesc = models.ForeignKey(Video)
+	videoExemplo = models.ForeignKey(Video)
+	videoVariac = models.ForeignKey(Video)
 
-	# id
-
-
-
-class Video()
+class Grupo(models.Model):
+	nome = models.CharField(max_length=30)
 
