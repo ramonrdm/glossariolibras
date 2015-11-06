7973e6c2b0357265e0dbeea65b826bbdc026987f
ramon.rdm <ramon.rdm@gmail.com>
2015-11-06 19:15:08 -0200

    mudanca field video

--- glossario/models.py
+++ glossario/models.py
@@ -1,13 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.db.models import FileField
 from django.core.files import File
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Localizacao(models.Model):
 	nome = models.CharField(max_length=30)
 	bsw = models.TextField()
 
-class Video(models.Model):
+class Video(FileField):
 	 capa = models.ImageField(blank=True)
 
 class Glossario(models.Model):
@@ -24,7 +25,8 @@ class Sinal(models.Model):
 	dataPost = models.DateField()
 	postador = models.ForeignKey(User)
 	publicado = models.BooleanField(default=False)
-	sinalLibras = models.ForeignKey(Video, related_name="sinal")
-	descLibras = models.ForeignKey(Video, related_name="descricao")
-	exemploLibras = models.ForeignKey(Video, related_name="exemplo")
-	variacaoLibras = models.ForeignKey(Video, related_name="variacao")
+	sinalLibras = Video()
+	# aqui deve ser uma field extendida, pois o arquivo vídeo eh uma field. Então faça a mesma coisa para os demais vídeos.
+	#descLibras = models.ForeignKey(Video, related_name="descricao")
+	#exemploLibras = models.ForeignKey(Video, related_name="exemplo")
+	#variacaoLibras = models.ForeignKey(Video, related_name="variacao")
--- glossariolibras/urls.py
+++ glossariolibras/urls.py
@@ -2,12 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'glossario.views.index', name='index'),
-
-
-   
     url(r'^admin/', include(admin.site.urls)),
 )+static(settings.STATIC_URL, document_root=settings.STATIC_URL)
