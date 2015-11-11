86798919a638de0f32a59c4d742d5c402d5cfad9
ramonrdm <ramon.rdm@gmail.com>
2015-11-11 17:50:44 -0200

    ajuste para exibir imagens

--- glossariolibras/urls.py
+++ glossariolibras/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
-admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'glossario.views.index', name='index'),
    
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
 )+static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
