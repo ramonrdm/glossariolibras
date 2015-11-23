d262ec115f6c6ea062a574f2b8408573a83a4a0c
joao pedro villegas <joao.pgkv@gmail.com>
2015-11-23 16:41:49 -0200

    layout ajustado e formulario de busca incompleto

--- glossario/forms.py
+++ glossario/forms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from glossario.models import Glossario
+from glossario.models import Glossario, Sinal
 from django import forms
 import datetime
 
@@ -9,6 +9,17 @@ class GlossarioForm(forms.ModelForm):
 	class Meta:
 		model = Glossario
 		exclude = ['link','dataCriacao']
+
+class PesquisaPortForm(forms.Form):
+
+	model = Sinal
+	fields = 'TraducaoP'
+	TraducaoP = forms.CharField(max_length=100)
+
+class PesquisaIngForm(forms.Form):
+	model = Sinal
+	fields = 'TraducaoI'
+	TraducaoI = forms.CharField(max_length=100)
 
 #	def clean_nome(self):
 #		palavra = self.cleaned_data['nome']
--- glossario/views.py
+++ glossario/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
-from glossario.models import Glossario
+from glossario.models import Glossario, Sinal
+from glossario.forms import PesquisaPortForm, PesquisaIngForm
 
 # Create your views here.
 
@@ -15,10 +16,25 @@ def index(request, glossario=None):
 
 	return render_to_response("index.html", dict(glossarios=glossarios, glossario=glossario))
 
-def pesquisa(request, glossario=None, tipopesq=None):
+def pesquisa(request, glossario=None, tipopesq=None, form=None):
+	if glossario:
+		try:
+			glossario = Glossario.objects.get(link=glossario)
+			if tipopesq	== "p":
+				form = PesquisaPortForm
+				return render_to_response("pesquisa.html", dict(glossario=glossario, form=form))
+			elif tipopesq =="e":
+				form = PesquisaIngForm
+				return render_to_response("pesquisa.html", dict(glossario=glossario, form=form))
+			elif tipopesq =="s":
+				form = None
+				return render_to_response("pesquisa.html", dict(glossario=glossario, form=form))
+		except Glossario.DoesNotExist:
+			glossarios = Glossario.objects.all()
+	else:
+		return render_to_response("pesquisa.html", dict(glossario=glossario, tipopesq=tipopesq))
 
 	return render_to_response("pesquisa.html", dict(glossario=glossario, tipopesq=tipopesq))
-			
 
 def equipe(request):
 
