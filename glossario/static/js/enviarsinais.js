616c15ec63e7bed6fc2a81feae1d16833b846219
Thiago Brezinski <thiagobrez@gmail.com>
2017-08-28 12:26:57 -0300

    Resolvido jQuery de hide/show do imagepicker. Adicionada classe hoverable nas options.

--- glossario/static/js/enviarsinais.js
+++ glossario/static/js/enviarsinais.js
@@ -1,19 +1,39 @@
 $(document).ready(function() {
 
-$('select').imagepicker({
-	show_label: true
-});
+	$('select').imagepicker({
+		show_label: true
+	});
 
-//EXIBIR
+	$('.thumbnail').addClass('hoverable');
 
-$('#id_localizacao .selected').click(function() {
-	$('#id_localizacao .thumbnail').show();
-});	
+	// HIDE/SHOW DO IMAGEPICKER
 
-//ESCONDER
+	let thumbnail_refs = [
+		{id: '#id_localizacao .thumbnail', opened: false},
+		{id: '#id_grupoCMe .thumbnail', opened: false},
+		{id: '#id_grupoCMd .thumbnail', opened: false},
+		{id: '#id_cmE .thumbnail', opened: false},
+		{id: '#id_cmD .thumbnail', opened:false}
+	];
 
-$('#id_localizacao .thumbnail:not(.selected)').click(function() {
-	$('#id_localizacao .thumbnail:not(.selected)').hide();
-});
+	let thumbnail_not_refs = [
+		'#id_localizacao .thumbnail:not(.selected)',
+		'#id_grupoCMe .thumbnail:not(.selected)',
+		'#id_grupoCMd .thumbnail:not(.selected)',
+		'#id_cmE .thumbnail:not(.selected)',
+		'#id_cmD .thumbnail:not(.selected)'
+	];
+
+	for(let i = 0; i < thumbnail_refs.length; i++){	
+		$(thumbnail_refs[i].id).click(function() {
+			if(thumbnail_refs[i].opened){
+				thumbnail_refs[i].opened = false;
+				$(thumbnail_not_refs[i]).hide();
+			} else {	
+				thumbnail_refs[i].opened = true;
+				$(thumbnail_not_refs[i]).show();
+			}
+		});
+	}
 
 });
