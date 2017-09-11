080a16373a8673a144b319317a8754646c40710f
Thiago Brezinski <thiagobrez@gmail.com>
2017-09-11 16:15:28 -0300

    Imagepickers na sidenav quase totalmente funcionais. Bug com cmE e cmD. Arrumar isso, aparência e lógica de busca.

--- glossario/static/js/baseSearchNav.js
+++ glossario/static/js/baseSearchNav.js
@@ -5,65 +5,51 @@ $(document).ready(function() {
 		show_label: true
 	});
 
-	// ADICIONA CLASSE HOVERABLE AOS THUMBNAILS
+	// ADICIONA EFEITO DE HOVER AOS THUMBNAILS
 	$('.thumbnail').addClass('hoverable');
 
-	// HIDE/SHOW IMAGEPICKER E AVATAR
-
-	let thumbnail_refs = [
-		{id: '#id_grupoCMe .thumbnail', opened: false},
-		{id: '#id_grupoCMd .thumbnail', opened: false},
-		{id: '#id_cmE .thumbnail', opened: false},
-		{id: '#id_cmD .thumbnail', opened:false}
+	let modal_refs = [
+		'#modalGrupoCMe',
+		'#modalGrupoCMd',
+		'#modalCMe',
+		'#modalCMd'
 	];
 
-	let thumbnail_not_refs = [
-		'#id_grupoCMe .thumbnail:not(.selected)',
-		'#id_grupoCMd .thumbnail:not(.selected)',
-		'#id_cmE .thumbnail:not(.selected)',
-		'#id_cmD .thumbnail:not(.selected)'
+	let select_refs = [
+		'#id_grupoCMe',
+		'#id_grupoCMd',
+		'#id_cmE',
+		'#id_cmD'
 	];
 
-	// let modal_refs = [
-	// 	'#modalGrupoCMe',
-	// 	'#modalGrupoCMd',
-	// 	'#id_cmE .thumbnail:not(.selected)',
-	// 	'#id_cmD .thumbnail:not(.selected)'
-	// ];
+	// ATUALIZA THUMBNAIL EXIBIDO NA SIDENAV QUANDO ALGUM É SELECIONADO NO MODAL
+	function sideNavThumbnailRefresh() {
+		for(let i = 0; i < modal_refs.length; i++){
+			$(modal_refs[i] + ' .thumbnail').click(function() {
+				let attrValue = $(this).find('p').html();
 
-	// let thumbnail_localizacao_ref = {id: '#id_localizacao .thumbnail', opened: false};
+				// REMOVE ATRIBUTO 'SELECTED' DA OPTION QUE JÁ ESTAVA SELECIONADA
+				$(".formSinais select" + select_refs[i] + " option[selected='selected']").removeAttr('selected');
 
-	// function showAvatar(){
-	// 	$(thumbnail_localizacao_ref.id).click(function() {
-	// 		if(thumbnail_localizacao_ref.opened){
-	// 			thumbnail_localizacao_ref.opened = false;
-	// 			$('.map').hide();
-	// 		} else {
-	// 			thumbnail_localizacao_ref.opened = true;
-	// 			$('.map').show();
-	// 		}
-	// 	});
-	// }
+				// ADICIONA ATRIBUTO 'SELECTED' À OPTION CLICADA
+				$(".formSinais select" + select_refs[i] + " option[value='" + attrValue + "']").attr('selected', 'selected');
 
-	for(let i = 0; i < thumbnail_refs.length; i++){	
-		$(thumbnail_refs[i].id).click(function() {
-			if(thumbnail_refs[i].opened){
-				thumbnail_refs[i].opened = false;
-				$(thumbnail_not_refs[i]).hide();
-				$(thumbnail_refs[i].id.split('.')[0] + 'li').click(function() {
-					$(this).parent().prepend($(thumbnail_refs[i].id.split('.')[0] + 'li:contains("Selecionar")'));
-					$(this).parent().prepend(this);
+				// RECONSTRÓI O IMAGEPICKER PARA ATUALIZAR THUMBNAIL EXIBIDO
+				$('select' + select_refs[i]).imagepicker({
+					show_label: true
 				});
-			} else {	
-				thumbnail_refs[i].opened = true;
-				// $(thumbnail_not_refs[i]).show();
-				// $(modal_refs[i]).modal('close');
-			}
-		});
+
+				// ADICIONA EFEITO DE HOVER AOS THUMBNAILS NOVAMENTE APOS RECONSTRUÇÃO DO IMAGEPICKER
+				$('.thumbnail').addClass('hoverable');
+
+				// VINCULA A THUMBNAIL DA SIDENAV AO EVENTO DE CLIQUE NOVAMENTE
+				sideNavThumbnailRefresh();
+			});
+		}
 	}
-	
-	// // INICIALIZA A FUNÇÃO PARA MOSTRAR O AVATAR
-	// showAvatar();
+
+	// INICIALIZA A FUNÇÃO PARA ATUALIZAR THUMBNAIL NA SIDENAV
+	sideNavThumbnailRefresh();
 
 	// INICIALIZA O IMAGEMAPSTER
 	$('#modeloImg').mapster( {
@@ -140,10 +126,6 @@ $(document).ready(function() {
 	// GERENCIA EVENTOS RELACIONADOS AO CLIQUE EM UMA ÁREA
 	$('area').click(function() {
 
-		// ESCONDE O AVATAR
-		// thumbnail_localizacao_ref.opened = false;
-		// $('.map').hide();
-
 		// VINCULA A ÁREA CLICADA À OPTION DA SUA LOCALIZAÇÃO
 		let attrValue = $(this).attr('data-key');
 		$("#id_localizacao option[selected='selected']").removeAttr('selected');
@@ -152,14 +134,13 @@ $(document).ready(function() {
 		// CORRIGE DESLOCAMENTO DO IMAGEPICKER
 		$('#id_localizacao option').parent().prepend($('#id_localizacao option[selected="selected"]'));
 
-		// RECONSTRÓI O IMAGEPICKER
+		// RECONSTRÓI O IMAGEPICKER PARA ATUALIZAR THUMBNAIL EXIBIDO
 		$('select#id_localizacao').imagepicker({
 			show_label: true
 		});
-		$('.thumbnail').addClass('hoverable');
 
-		// VINCULA O IMAGEPICKER AO EVENTO DE MOSTRAR O AVATAR NOVAMENTE
-		// showAvatar();
+		// ADICIONA EFEITO DE HOVER AOS THUMBNAILS NOVAMENTE APOS RECONSTRUÇÃO DO IMAGEPICKER
+		$('.thumbnail').addClass('hoverable');
 
 	});
 
