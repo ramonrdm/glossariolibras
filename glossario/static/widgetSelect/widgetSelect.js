c5e1776e7c19fa288f9306abdd41e64a2aeea7b6
Cleberton Oliveira <clebertonoliveira.cnt@gmail.com>
2019-07-11 20:53:18 -0300

    Colocando input no novo select

--- glossario/static/widgetSelect/widgetSelect.js
+++ glossario/static/widgetSelect/widgetSelect.js
@@ -1,20 +1,26 @@
 var iconSelect;
+var selectedText;
 
 window.onload = function(){
 
+    selectedText = document.getElementById('selected-text');
+    document.getElementById('my-icon-select').addEventListener('changed', function(e){
+       selectedText.value = iconSelect.getSelectedValue();
+    });
+
     iconSelect = new IconSelect("movimentacao-widget",
-        {'selectedIconWidth':100,
-        'selectedIconHeight':100,
-        'selectedBoxPadding':5,
+        {'selectedIconWidth':90,
+        'selectedIconHeight':90,
+        'selectedBoxPadding':0,
         'iconsWidth':50,
         'iconsHeight':50,
-        'boxIconSpace':2,
+        'boxIconSpace':0,
         'vectoralIconNumber':1,
         'horizontalIconNumber':2});
 
     var icons = [];
 
-    icons.push({'iconFilePath':'/static/img/X.svg', 'valur':'0'});
+    icons.push({'iconFilePath':'/static/img/X.svg', 'iconValue':'0'});
     icons.push({'iconFilePath':'/static/img/parede.png', 'iconValue':'1'});
     icons.push({'iconFilePath':'/static/img/chao.png', 'iconValue':'2'});
     icons.push({'iconFilePath':'/static/img/circular.png', 'iconValue':'3'});
