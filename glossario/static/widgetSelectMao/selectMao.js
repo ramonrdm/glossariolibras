9247247790d949d838f1bc544ccf2ccaf4f837fa
Cleberton Oliveira <clebertonoliveira.cnt@gmail.com>
2019-08-14 12:32:16 -0300

    Mudanças em nomes das variaveis do css e js WidgetSelectMao, porem ainda não funcionando junto com o WidgetSelectMovimentacao

--- glossario/static/widgetSelectMao/selectMao.js
+++ glossario/static/widgetSelectMao/selectMao.js
@@ -27,25 +27,22 @@ iconSelectMao.DEFAULT.BOX_ICON_SPACE = 1;
 iconSelectMao.DEFAULT.HORIZONTAL_ICON_NUMBER = 3;
 iconSelectMao.DEFAULT.VECTORAL_ICON_NUMBER = 3;
 
-
-
-
-
+selectedMao
 function iconSelectMao($$elementID, $$parameters) {
-    
+
     var _icons = [];
     var _selectedIndex = -1;
     var _boxScroll;
-    
+
     var _default = iconSelectMao.DEFAULT;
 
     function _init() {
-        
+
         //parametreler boş gelirse
         if(!$$parameters) $$parameters = {};
         //En üst elementi seç
         if(_View.setIconSelectElement($$elementID)){
-            
+
             //set parameters
             $$parameters = _Model.checkParameters($$parameters);
             //create UI
@@ -54,34 +51,34 @@ function iconSelectMao($$elementID, $$parameters) {
             _View.iconSelectElement.onclick = function(){
                 _View.showBox();
             };
-            
+
             //Başlangıçta gizle
             _View.showBox(false);
 
             //Nesneye basıldığında gizlemeyi iptal et.
             _View.iconSelectElement.addEventListener('click', function($event){
-                $event.stopPropagation();             
+                $event.stopPropagation();
             });
-            
+
             //dışarı basıldığında gizle.
             window.addEventListener('click', function(){
                 _View.showBox(false);
             });
-           
+
         }else{
             alert("Element not found.");
         }
-        
+
     }
-    
+
     //Tüm iconları yeniden yükle.
     this.refresh = function($icons){
-        
+
         _icons = [];
-        
+
         var setSelectedIndex = this.setSelectedIndex;
         var setOpenCM = this.setOpenCM
-        
+
         for(var i = 0; i < $icons.length; i++){
             $icons[i].element = _View.createIcon($icons[i].iconFilePathMao, $icons[i].iconValueMao, i, $$parameters);
             $icons[i].element.onmouseover = function(){
@@ -90,25 +87,25 @@ function iconSelectMao($$elementID, $$parameters) {
 
             };
             _icons.push($icons[i]);
-            
+
         }
-        
+
         var horizontalIconNumber = Math.round(($icons.length) / $$parameters.vectoralIconNumber);
-        
-        _View.boxElement.style.height = (($$parameters.iconsHeight + 2) * horizontalIconNumber) + 
+
+        _View.boxElement.style.height = (($$parameters.iconsHeight + 2) * horizontalIconNumber) +
                 ((horizontalIconNumber + 1) * $$parameters.boxIconSpace);
         this.setSelectedIndex(0);
-        
+
     };
-    
+
     //icon listesini al.
     this.getIcons = function(){ return _icons; };
-    
+
     //iconu seçili hale gelir.
     this.setSelectedIndex = function($index){
-        
+
         var iconMao;
-        
+
         if(_icons.length > $index)
             iconMao = _icons[$index];
 
@@ -117,13 +114,13 @@ function iconSelectMao($$elementID, $$parameters) {
             if(_selectedIndex != -1) _icons[_selectedIndex].element.setAttribute('class','iconMao');
             _selectedIndex = $index;
             _View.selectedIconImgElement.setAttribute('src', icon.iconFilePathMao);
-            if(_selectedIndex != -1) _icons[_selectedIndex].element.setAttribute('class','iconMao selected');
+            if(_selectedIndex != -1) _icons[_selectedIndex].element.setAttribute('class','iconMao selectedMao');
         }
-        
+
         _View.iconSelectElement.dispatchEvent(new Event('changedmao'));
-        
+
         //_View.showBox(false);
-        
+
     };
 
 //       this.setOpenCM = function($index){
@@ -147,29 +144,29 @@ function iconSelectMao($$elementID, $$parameters) {
 //
 //    };
 
-    
+
     this.getSelectedIndex = function(){ return _selectedIndex; };
     this.getSelectedValue = function(){ return _icons[changedmao].iconValueMao };
     this.getSelectedFilePath = function(){ return _icons[_selectedIndex].iconFilePathMao };
-    
-    
-    
+
+
+
     //### VIEW CLASS ###
-        
+
     function _View(){}
-    
+
     _View.iconSelectElement;
     _View.boxElement;
     _View.boxScrollElement;
     _View.selectedIconImgElement;
     _View.selectedIconElement;
-    
+
     _View.showBox = function($isShown){
-                
+
          if($isShown == null) {
              $isShown = (_View.boxElement.style.display == "none") ? true : false;
          }
-                
+
         if($isShown) {
             _View.boxElement.style.display = "block";
             _View.boxScrollElement.style.display = "block";
@@ -178,30 +175,30 @@ function iconSelectMao($$elementID, $$parameters) {
             _View.boxElement.style.display = "none";
             _View.boxScrollElement.style.display = "none";
         }
-        
+
         _View.boxElement.style.display = ($isShown) ? "block" : "none";
-        
-        
-            
+
+
+
     };
-    
+
     _View.setIconSelectElement = function($elementID){
         _View.iconSelectElement = document.getElementById($elementID);
         return _View.iconSelectElement;
     };
-    
+
     _View.clearUI = function(){
         _View.iconSelectElement.innerHTML = "";
     };
-    
+
     _View.clearIcons = function(){
         _View.boxElement.innerHTML = "";
     };
-    
+
     _View.createUI = function($parameters){
-        
+
         /* HTML MODEL
-        
+
         <div id="my-icon-select" class="icon-select">
             <div class="selected-box">
                 <div class="selected-icon"><img src="images/icons/i2.png"></div>
@@ -220,39 +217,39 @@ function iconSelectMao($$elementID, $$parameters) {
                 </div>
             </div>
         </div>
-        
+
         */
-        
+
         _View.clearUI();
-        
+
         _View.iconSelectElement.setAttribute('class', 'icon-selectMao');
-        
+
         var selectedBoxElement = document.createElement('div');
         selectedBoxElement.setAttribute('class' ,'selected-boxMao');
-        
+
         var selectedIconElement = document.createElement('div');
         selectedIconElement.setAttribute('class' ,'selected-iconMao');
-        
+
         _View.selectedIconImgElement = document.createElement('img');
         _View.selectedIconImgElement.setAttribute('src', '');
         selectedIconElement.appendChild(_View.selectedIconImgElement);
-        
+
         var componentIconElement = document.createElement('div');
         componentIconElement.setAttribute('class', 'component-iconMao');
-        
+
         var componentIconImgElement = document.createElement('img');
         componentIconImgElement.setAttribute('src', iconSelectMao.COMPONENT_ICON_FILE_PATH );
         componentIconElement.appendChild(componentIconImgElement);
-        
+
         _View.boxScrollElement = document.createElement('div');
         _View.boxScrollElement.setAttribute('id',$$elementID + "-box-scroll");
         _View.boxScrollElement.setAttribute('class', 'boxMao');
-        
+
         _View.boxElement = document.createElement('div');
-        
+
         //_View.boxElement.setAttribute('class', 'box');
         _View.boxScrollElement.appendChild(_View.boxElement);
-        
+
         _View.selectedIconImgElement.setAttribute('width', $parameters.selectedIconWidth);
         _View.selectedIconImgElement.setAttribute('height', $parameters.selectedIconHeight);
         selectedIconElement.style.width = $parameters.selectedIconWidth;
@@ -262,23 +259,23 @@ function iconSelectMao($$elementID, $$parameters) {
         selectedIconElement.style.top = $parameters.selectedBoxPadding;
         selectedIconElement.style.left = $parameters.selectedBoxPadding;
         componentIconElement.style.bottom = 4 + $parameters.selectedBoxPadding;
-        
+
         _View.boxScrollElement.style.left = parseInt(selectedBoxElement.style.width) + 1;
-        
-        _View.boxScrollElement.style.width = (($parameters.iconsWidth + 2) * $parameters.vectoralIconNumber) + 
+
+        _View.boxScrollElement.style.width = (($parameters.iconsWidth + 2) * $parameters.vectoralIconNumber) +
                 (($parameters.vectoralIconNumber + 1) * $parameters.boxIconSpace);
-        _View.boxScrollElement.style.height = (($parameters.iconsHeight + 2) * $parameters.horizontalIconNumber) + 
+        _View.boxScrollElement.style.height = (($parameters.iconsHeight + 2) * $parameters.horizontalIconNumber) +
                 (($parameters.horizontalIconNumber + 1) * $parameters.boxIconSpace);
-         
+
         _View.boxElement.style.left = _View.boxScrollElement.style.left;
         _View.boxElement.style.width = _View.boxScrollElement.style.width;
-        
+
         _View.iconSelectElement.appendChild(selectedBoxElement);
         selectedBoxElement.appendChild(selectedIconElement);
         selectedBoxElement.appendChild(componentIconElement);
         selectedBoxElement.appendChild(_View.boxScrollElement);
-        
-        
+
+
         var results = {};
         results['iconSelectElement'] = _View.iconSelectElement;
         results['selectedBoxElement'] = selectedBoxElement;
@@ -286,50 +283,50 @@ function iconSelectMao($$elementID, $$parameters) {
         results['selectedIconImgElement'] = _View.selectedIconImgElement;
         results['componentIconElement'] = componentIconElement;
         results['componentIconImgElement'] = componentIconImgElement;
-        
+
         return results;
-        
-        
+
+
        //trigger: created ( run setValues )
-        
+
     };
-        
+
     _View.createIcon = function($iconFilePathMao, $iconValueMao, $index, $parameters){
-        
-        /* HTML MODEL 
-         
+
+        /* HTML MODEL
+
          <div class="icon"><img src="images/icons/i1.png"></div>
-         
+
          */
-        
+
         var iconElement = document.createElement('div');
         iconElement.setAttribute('class', 'icon');
         iconElement.style.width = $parameters.iconsWidth;
         iconElement.style.height = $parameters.iconsHeight;
         iconElement.style.marginLeft = $parameters.boxIconSpace;
         iconElement.style.marginTop = $parameters.boxIconSpace;
-        
+
         var iconImgElement = document.createElement('img');
         iconImgElement.setAttribute('src', $iconFilePathMao);
         iconImgElement.setAttribute('icon-value', $iconValueMao);
         iconImgElement.setAttribute('icon-index', $index);
         iconImgElement.setAttribute('width', $parameters.iconsWidth);
         iconImgElement.setAttribute('height', $parameters.iconsHeight);
-        
+
         iconElement.appendChild(iconImgElement);
         _View.boxElement.appendChild(iconElement);
-        
+
         return iconElement;
-        
+
     };
-    
+
     //### MODEL CLASS ###
-    
+
     function _Model(){}
-    
+
     //TODO: params değişkenini kaldır yeni oluştursun.
     _Model.checkParameters = function($parameters){
-        
+
         $parameters.selectedIconWidth          = ($parameters.selectedIconWidth)          ? $parameters.selectedIconWidth        : _default.SELECTED_ICON_WIDTH;
         $parameters.selectedIconHeight         = ($parameters.selectedIconHeight)         ? $parameters.selectedIconHeight       : _default.SELECTED_ICON_HEIGHT;
         $parameters.selectedBoxPadding         = ($parameters.selectedBoxPadding)         ? $parameters.selectedBoxPadding       : _default.SELECTED_BOX_PADDING;
@@ -339,11 +336,11 @@ function iconSelectMao($$elementID, $$parameters) {
         $parameters.boxIconSpace               = ($parameters.boxIconSpace)               ? $parameters.boxIconSpace             : _default.BOX_ICON_SPACE;
         $parameters.vectoralIconNumber         = ($parameters.vectoralIconNumber)         ? $parameters.vectoralIconNumber       : _default.VECTORAL_ICON_NUMBER;
         $parameters.horizontalIconNumber       = ($parameters.horizontalIconNumber)       ? $parameters.horizontalIconNumber     : _default.HORIZONTAL_ICON_NUMBER;
-    
+
         return $parameters;
-    
+
     };
-    
+
     _init();
-    
+
 }
