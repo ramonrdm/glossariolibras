b99834917573317de382524964cc421fb9709367
Cleberton Oliveira <clebertonoliveira.cnt@gmail.com>
2019-08-20 17:39:51 -0300

    Tentando pegar o ID certo no JS

--- glossario/static/js/modalCM.js
+++ glossario/static/js/modalCM.js
@@ -1,4 +1,15 @@
   $(document).ready(function(){
     $('.modalCM').modal();
     $('.modalGrupos').modal();
-  });
+
+
+
+    $('#modalGrupos').click(function(){
+        var images = $('#modal-content img').attr('id');
+        alert(images);
+    });
+
+
+ });
+
+
